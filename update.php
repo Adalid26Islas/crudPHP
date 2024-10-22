1f1ac11dcb6a62d0d4978e15e98016e0ebe75d83
Adalid26Islas <adalidislasquintero@gmail.com>
2024-10-21 20:05:39 -0600

    Creacion de los archivos necesarios para actualizar los registros

--- update.php
+++ update.php
@@ -1,3 +1,8 @@
+<?php
+    include "modelo/db.php";
+    $id=$_GET["id"];
+    $sql=$conexion->query("select * from personas where ID=$id");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,27 +15,33 @@
 <body> 
     <form class="col-4 p-2 m-auto" method="POST">
         <h3 class="text-center alert alert-secondary p-2 mt-3">Actualización de persona</h3> 
+        <input type="hidden" name="id" value="<?= $_GET["id"]?>">
+        <?php
+        include "controlador/update_person.php";
+        while ($datos= $sql->fetch_object()) { ?>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Nombre de la pesona</label>
-            <input type="text" class="form-control" name="nombre">
+            <input type="text" class="form-control" name="nombre" value="<?= $datos->nombre?>">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Apellidos</label>
-            <input type="text" class="form-control" name="apellidos">
+            <input type="text" class="form-control" name="apellidos" value="<?= $datos->apellidos?>">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">DNI de la persona</label>
-            <input type="text" class="form-control" name="dni">
+            <input type="text" class="form-control" name="dni" value="<?= $datos->dni?>">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Fecha de nacimiento</label>
-            <input type="date" class="form-control" name="fecha">
+            <input type="date" class="form-control" name="fecha" value="<?= $datos->fecha_nacimiento?>">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Correo electronico</label>
-            <input type="email" class="form-control" name="correo">
+            <input type="email" class="form-control" name="correo" value="<?= $datos->email?>">
         </div>
-        <button type="submit" class="btn btn-success" name="btnregistrar" value="ok">Registrar Persona</button>
+        <?php }
+        ?>
+        <button type="submit" class="btn btn-success" name="btnmodificar" value="ok">Actualizar Persona</button>
     </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
